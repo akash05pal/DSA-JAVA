5859f29e12dcbb8e952d18fee2c05814d90e2ebf
Akash pal <skypal831@gmail.com>
2025-02-11 00:02:55 +0530

    code updated

--- gfg potd/Prefixsum2.java
+++ gfg potd/Prefixsum2.java
@@ -1,16 +1,32 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Prefixsum2 {
-    public static void main(String[] args) {  // Corrected method signature
-        int[] arr = {1, 2, 3, 4, 5};
-        int n = arr.length;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking input for array size
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
         int[] prefixSum = new int[n];
 
-        arr[0] = arr[0];
-        for (int i = 1; i < n; i++) {
-            arr[i] = arr[i - 1] + arr[i];
+        // Taking input for array elements
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
         }
 
-        System.out.println("Prefix Sum: " + Arrays.toString(arr));
+        // Computing prefix sum
+        prefixSum[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        }
+
+        // Printing the prefix sum array
+        System.out.println("Prefix Sum: " + Arrays.toString(prefixSum));
+
+        scanner.close();
     }
 }
