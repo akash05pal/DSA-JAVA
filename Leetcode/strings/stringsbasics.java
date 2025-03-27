926338a0af9c447e0cfe16a1f9db011eb0cbf602
Akash pal <skypal831@gmail.com>
2025-03-28 02:01:37 +0530

    upd

---
+++ Leetcode/strings/stringsbasics.java
@@ -0,0 +1,10 @@
+//package strings; 
+
+public class stringsbasics {
+    public static void main(String[] args) {
+        String str ="akash ";
+        System.out.println(str);
+        System.out.println(str.length());
+        
+    }
+}
